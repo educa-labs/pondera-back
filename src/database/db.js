38133f6b345b7121eea135a71dbd3677f173b155
Andres <afcadiz@uc.cl>
2017-11-08 12:47:42 -0300

    fixed linter

--- src/database/db.js
+++ src/database/db.js
@@ -1,19 +1,19 @@
 const pgp = require('pg-promise')();
 
-DB_NAME = "tuniversidad_production";
-DB_USER = "pguser";
-DB_PASS = "tuniversidad";
-DB_HOST = "192.241.172.51";
-DB_PORT = "5432";
+DB_NAME = 'tuniversidad_production';
+DB_USER = 'pguser';
+DB_PASS = 'tuniversidad';
+DB_HOST = '192.241.172.51';
+DB_PORT = '5432';
 
-DBP_NAME = "pondera";
-DBP_USER = "educalabs";
-DBP_PASS = "Callampa123123";
-DBP_HOST = "pondera.cjg6fuesbuu0.us-east-2.rds.amazonaws.com";
-DBP_PORT = "5432";
+DBP_NAME = 'pondera';
+DBP_USER = 'educalabs';
+DBP_PASS = 'Callampa123123';
+DBP_HOST = 'pondera.cjg6fuesbuu0.us-east-2.rds.amazonaws.com';
+DBP_PORT = '5432';
 
 
 const db_tuni = pgp(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 const db_pond = pgp(`postgres://${DBP_USER}:${DBP_PASS}@${DBP_HOST}:${DBP_PORT}/${DBP_NAME}`);
 
-module.exports = {"db_tuni": db_tuni, "db_pond": db_pond};
+module.exports = { db_tuni, db_pond };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,49 +1,49 @@
-var express = require('express');
-var router = express.Router();
-var parameters = require('parameters-middleware');
-var db = require('../database/db');
-var crypt = require('crypto');
-var randomstring = require("randomstring");
+const express = require('express');
+
+const router = express.Router();
+const parameters = require('parameters-middleware');
+const db = require('../database/db');
+const crypt = require('crypto');
+const randomstring = require('randomstring');
 
 // In case of losing password
 // randomstring.generate();
 
 // Parameters
 function getMessage(missing) {
-  return 'Missing params: ' + missing.join(', ');
+  return `Missing params: ${missing.join(', ')}`;
 }
 
-var userParams = parameters({body: ["name", "mail", 
-"password", "rut", "phone", "city"]},
-{message: getMessage},
-{statusCode: 400}
-)
+const userParams = parameters(
+  {
+    body: ['name', 'mail',
+      'password', 'rut', 'phone', 'city'],
+  },
+  { message: getMessage },
+  { statusCode: 400 },
+);
 
 // ROUTES
 /* CREATE new user */
-router.post('/', userParams, function(req, res, next) {
-  var j = req.body
-  var pswd = encryptPasswd(j.password)
+router.post('/', userParams, (req, res, next) => {
+  const j = req.body;
+  const pswd = encryptPasswd(j.password);
   db.db_pond.one(`INSERT INTO
                   Users(name, mail, password_digest,
                   rut, phone, city)
                   VALUES($1, $2, $3, $4, $5, $6) RETURNING id, password_digest;  
                   `, [j.name, j.mail, pswd, j.rut, j.phone, j.city])
-  .then(function(data){
+    .then((data) => {
       res.status(200)
-      .json({
-          "message": "Usuario creado correctamente",
-          "data": data
-      })
-  })
-  .catch(function(obj){
-    res.json({"message": "hola"})
-  });
+        .json({
+          message: 'Usuario creado correctamente',
+          data,
+        });
+    })
+    .catch((obj) => {
+      res.json({ message: 'hola' });
+    });
 });
-
-
-
-
 
 
 function encryptPasswd(data) {
