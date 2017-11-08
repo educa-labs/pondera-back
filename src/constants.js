0c62ced80baaa6bcfa85d816455e42c7d4a81483
Andres <afcadiz@uc.cl>
2017-11-08 13:14:17 -0300

    added env variable constants support with default values

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  DB_NAME: process.env.DB_NAME || 'tuniversidad_production',
+  DB_USER: process.env.DB_USER || 'pgadmin',
+  DB_PASS: process.env.DB_PASS || 'tuniversidad',
+  DB_HOST: process.env.DB_HOST || '192.241.172.51',
+  DB_PORT: process.env.DB_PORT || '5432',
+  DBP_NAME: process.env.DBP_NAME || 'pondera',
+  DBP_USER: process.env.DBP_USER || 'educalabs',
+  DBP_PASS: process.env.DBP_PASS || 'Callampa123123',
+  DBP_HOST: process.env.DBP_HOST || 'pondera.cjg6fuesbuu0.us-east-2.rds.amazonaws.com',
+  DBP_PORT: process.env.DBP_PORT || '5432',
+};
--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,8 @@
 const pgp = require('pg-promise')();
 
-DB_NAME = 'tuniversidad_production';
-DB_USER = 'pguser';
-DB_PASS = 'tuniversidad';
-DB_HOST = '192.241.172.51';
-DB_PORT = '5432';
-
-DBP_NAME = 'pondera';
-DBP_USER = 'educalabs';
-DBP_PASS = 'Callampa123123';
-DBP_HOST = 'pondera.cjg6fuesbuu0.us-east-2.rds.amazonaws.com';
-DBP_PORT = '5432';
-
+const {
+  DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT, DBP_NAME, DBP_USER, DBP_PASS, DBP_HOST, DBP_PORT,
+} = require('../constants');
 
 const db_tuni = pgp(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 const db_pond = pgp(`postgres://${DBP_USER}:${DBP_PASS}@${DBP_HOST}:${DBP_PORT}/${DBP_NAME}`);
