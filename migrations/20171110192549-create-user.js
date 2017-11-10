345259c90440c7e82f8c63e673dcb3c1d9dff4c5
Andres <afcadiz@uc.cl>
2017-11-10 17:11:37 -0300

    added sequelize integration, created user model and updated sessions and user routes code

---
+++ migrations/20171110192549-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      mail: {
+        type: Sequelize.STRING
+      },
+      password_digest: {
+        type: Sequelize.STRING
+      },
+      hash: {
+        type: Sequelize.STRING
+      },
+      rut: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
--- src/routes/session.js
+++ src/routes/session.js
@@ -5,6 +5,7 @@ const db = require('../database/db');
 const randomstring = require('randomstring');
 const parameters = require('parameters-middleware');
 const crypt = require('crypto');
+const models = require('../../models')
 
 // randomstring.generate();
 // Parameters requirements
@@ -22,27 +23,21 @@ const loginParams = parameters(
 router.post('/', loginParams, (req, res, next) => {
   const j = req.body;
   const paswd = encryptPasswd(j.password);
-  db.db_pond.one(`SELECT password_digest FROM Users WHERE mail=$1;
-                `, [j.mail])
-    .then((data) => {
-      // Comprobar password
-      console.log(paswd);
-      console.log(data.password_digest);
-      if (data.password_digest === paswd) {
-        // Generar nuevo token
-        const newToken = randomstring.generate();
-        db.db_pond.one(`UPDATE Users 
-                        SET token = $1 
-                        WHERE mail = $2`, [newToken, j.mail]);
-        res.status(201).json({ message: 'Sesión creada correctamente', token: newToken });
-      } else {
-        res.status(401).json({ message: 'Credenciales invalidas' });
-      }
-    })
-    .catch((obj) => {
-      console.log(obj);
+  models.User.findOne({ where: { mail: j.mail } }).then((data) => {
+    // Comprobar password
+    if (data.password_digest === paswd) {
+      // Generar nuevo token
+      const newToken = randomstring.generate();
+      data.token = newToken;
+      data.save();
+      res.status(201).json({ message: 'Sesión creada correctamente', token: newToken });
+    } else {
       res.status(401).json({ message: 'Credenciales invalidas' });
-    });
+    }
+  }).catch((obj) => {
+    console.log(obj);
+    res.status(401).json({ message: 'Credenciales invalidas' });
+  });
 });
 
 
