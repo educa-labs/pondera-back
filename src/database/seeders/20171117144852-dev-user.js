caa96cab0eb1eba510373a9bb438af9e4d4f66d2
Andres <afcadiz@uc.cl>
2017-11-17 14:56:08 -0300

    added user seed

--- src/database/seeders/20171117144852-dev-user.js
+++ src/database/seeders/20171117144852-dev-user.js
@@ -1,11 +1,16 @@
+const encrypt = require('../../helpers/session');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
       name: 'don felipe',
-      mail: 'a@.cl',
+      mail: 'a@a.cl',
       rut: '19291235-0',
       phone: '+569569569',
       city: 'wuat wea',
+      password_digest: encrypt.encryptPasswd('123123'), 
+      createdAt: new Date(),
+      updatedAt: new Date(),
     }], {});
   },
 
