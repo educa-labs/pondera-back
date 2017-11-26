60da7cd0b2c75ae2b39b580bf9abd8bb658efdc8
Felipe Haase <fahaase@uc.cl>
2017-11-26 01:52:52 -0300

    final ponderations data

--- src/helpers/careers.js
+++ src/helpers/careers.js
@@ -1,7 +1,7 @@
 const db = require('../database/db');
 const rp = require('request-promise');
 
-function similarCareers(cId) {
+async function similarCareers(cId) {
   const options = {
     method: 'POST',
     uri: 'http://newton.tuniversidad.cl/get_nn',
@@ -12,21 +12,32 @@ function similarCareers(cId) {
     json: true, // Automatically stringifies the body to JSON
   };
   let ids;
-  const a = rp(options)
+  let id1;
+  let id2;
+  let id3;
+  console.log('HOLA');
+  await rp(options)
     .then((parsedBody) => {
       ids = parsedBody.result['0'];
-      const [id1, id2, id3] = ids;
-      db.db_tuni.any('SELECT carreers.id, carreers.title FROM carreers WHERE id = ${id1} OR id = ${id2} OR id = ${id3}', { id1, id2, id3 })
-        .then((data) => {
-          return data;
-        })
-        .catch((err) => {
-          return 'Error';
-        });
+      [id1, id2, id3] = ids;
     })
     .catch((err) => {
-      ids = ['error'];
+      
     });
+  let carreras = {};
+  await db.db_tuni.any(`SELECT carreers.id as "cId", universities.id as "uId", 
+  carreers.title as "careerTitle", universities.initials as "universityInitials" 
+  FROM carreers,universities
+  WHERE universities.id = carreers.university_id
+  AND (carreers.id = ${id1} OR carreers.id = ${id2} OR carreers.id = ${id3})`, { id1, id2, id3 })
+    .then((data) => {
+      carreras = data;
+    })
+    .catch((err) => {
+      console.log(err);
+      return 'Error';
+    });
+  return carreras;
 }
 
 module.exports = { similarCareers };
