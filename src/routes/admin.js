da1686e28214afbadb243a16be3d17a5d6041e6d
Felipe Haase <fahaase@uc.cl>
2017-12-07 12:10:05 -0300

    fixed unique user of ponderations for stats

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -19,7 +19,7 @@ router.get('/users', (req, res, next) => {
 
 router.get('/stats', (req, res, next) => {
   const data = db.db_pond.any(`SELECT "Careers".id as cId, "Universities".title as uTitle, 
-  "Careers".title as cTitle, count(*) as count
+  "Careers".title as cTitle, count(DISTINCT "Ponderations"."userId") as count
   FROM "Universities", "Careers", "Ponderations"
   WHERE "Universities".id = "Ponderations"."universityId" 
   AND "Careers".id = "Ponderations"."careerId" 
