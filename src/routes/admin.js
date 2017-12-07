33f0aa13976994f5960fe5dfdc7f4d2266b2b50a
Felipe Haase <fahaase@uc.cl>
2017-12-07 13:10:28 -0300

    added Ccreate admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,17 +3,33 @@ const db = require('../database/db');
 const models = require('../models');
 const config = require('../../config/config.js');
 const router = express.Router();
+const params = require('../helpers/parameters')
+const session = require('../helpers/session')
 
 
 router.get('/users', (req, res, next) => {
 
 });
 
-router.get('/session', (req, res, next) => {
-  
+router.post('/new', session.checkSuperadmin, (req, res, next) => {
+  const { mail } = req.body;
+  models.User.findOne({ where: { mail } })
+    .then((user) => {
+      if (user) {
+        user.admin = true;
+        user.save();
+        res.status(201).json({ message: 'Nuevo admin creado' });
+      }
+      else {
+        res.status(422).json({ message: 'Usuario no existe' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error' });
+    });
 });
 
-router.get('/stats', (req, res, next) => {
+router.get('/stats', session.checkAdmin, (req, res, next) => {
   const data = db.db_pond.any(`SELECT "Careers".id as cId, "Universities".title as uTitle, 
   "Careers".title as cTitle, count(DISTINCT "Ponderations"."userId") as count
   FROM "Universities", "Careers", "Ponderations"
