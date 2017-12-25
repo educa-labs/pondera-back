9da1452abd8f71f5959985a9f2717a20d9825cff
Felipe Haase <fahaase@uc.cl>
2017-12-25 20:23:54 -0300

    moved credentials to environment vars

--- src/app.js
+++ src/app.js
@@ -33,8 +33,8 @@ mailer.extend(app, {
   port: 465, // port for secure SMTP
   transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
   auth: {
-    user: 'soporte@educalabs.cl',
-    pass: 'callampa123',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
   }
 });
 
